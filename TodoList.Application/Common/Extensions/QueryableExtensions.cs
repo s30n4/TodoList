ba6c74b7972dd77f3bf0dcb9e441a4d7cfd1dbea
Sina Akbari <sina.akbari.victorion@outlook.com>
2021-04-11 18:00:17 +1000

    Add QueryableExtenstions to OrderBy queries by column name

---
+++ TodoList.Application/Common/Extensions/QueryableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Dynamic.Core;
+using System.Linq;
+using System.Text;
+using TodoList.Application.Common.Extensions;
+
+namespace TodoList.Application.Common.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> query, string orderByQueryString, bool orderByDescending)
+        {
+            if (!query.Any())
+                return query;
+
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return query;
+            }
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+                if (!param.EndsWith("descending") && !param.EndsWith("ascending"))
+                {
+                    var sortingOrder = orderByDescending ? "descending" : "ascending";
+
+                    orderQueryBuilder.Append($"{param} {sortingOrder}, ");
+                }
+                else
+                {
+                    orderQueryBuilder.Append($"{param}, ");
+                }
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            return query.OrderBy(orderQuery);
+        }
+
+        public static IQueryable<TSource> SkipToPage<TSource>(this IQueryable<TSource> source, int pageNumber, int pageSize)
+        {
+            int startRowIndex = (pageNumber - 1) * pageSize;
+
+            return source.Skip(startRowIndex).Take(pageSize);
+        }
+    }
+
+}

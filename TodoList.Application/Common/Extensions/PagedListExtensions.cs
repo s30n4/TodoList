08155ea52a377c2bf4f4f908bc9620dafc012f0b
Sina Akbari <sina.akbari.victorion@outlook.com>
2021-04-11 17:59:40 +1000

    And PageListExtension for paging queries

---
+++ TodoList.Application/Common/Extensions/PagedListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TodoList.Application.Common.Concretes;
+using TodoList.Application.Common.Interfaces;
+
+namespace TodoList.Application.Common.Extensions
+{
+    public static class PagedListExtensions
+    {
+        public static IPagedList<T> ToPagedList<T>(this IEnumerable<T> list, int pageIndex, int pageSize, int totalCount)
+        {
+            return new PagedList<T>(list, pageIndex, pageSize, totalCount);
+        }
+    }
+}

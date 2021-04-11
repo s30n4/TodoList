9d7fd42d242f42c336291b0123e9299042d04bf5
Sina Akbari <sina.akbari.victorion@outlook.com>
2021-04-11 18:03:46 +1000

    Add TodoListItem Entity

---
+++ TodoList.Domain/TodoListManagement/Entities/TodoListItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TodoList.Domain.Abstracts;
+
+namespace TodoList.Domain.TodoListManagement.Entities
+{
+    public class TodoListItem : Entity
+    {
+        public Guid TodoListItemId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime? DueDate { get; private set; }
+
+        public TodoListItemStatuses Status { get; private set; }
+
+        public DateTime? CompletedDateTime { get; private set; }
+
+        public TodoListItem()
+        {
+
+        }
+
+        public TodoListItem(string name, string description, DateTime? dueDate)
+        {
+            TodoListItemId = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+            Status = TodoListItemStatuses.Pending;
+        }
+
+        public void Update(string name, string description, DateTime? dueDate)
+        {
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+        }
+
+        public void MarkAsDone()
+        {
+            if (!CompletedDateTime.HasValue)
+            {
+                CompletedDateTime = DateTime.UtcNow;
+                Status = TodoListItemStatuses.Done;
+            }
+        }
+    }
+}
---
+++ TodoList.Domain/TodoListManagement/Entities/TodoListItemStatuses.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TodoList.Domain.TodoListManagement.Entities
+{
+    public enum TodoListItemStatuses
+    {
+        [Description("Pending")]
+        Pending = 1,
+
+        [Description("Done")]
+        Done = 2,
+
+     
+    }
+}

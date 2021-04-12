261cf7e192b43e2cae26c4dc1e6d4da82b80d0ed
Sina Akbari <sina.akbari.victorion@outlook.com>
2021-04-13 05:40:24 +1000

    Adding Add/Update/Delete and Mark as done methods to TodoListItemController

--- TodoList.Api/Controllers/TodoListController.cs
+++ TodoList.Api/Controllers/TodoListController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using TodoList.Application.TodoLists.Commands;
 using TodoList.Application.TodoLists.Queries;
 
 namespace TodoList.Api.Controllers
@@ -31,6 +32,48 @@ namespace TodoList.Api.Controllers
         public async Task<ActionResult> GetDoneTodoListItems([FromQuery] GetTodoListItemsQuery request)
         {
             request.Status = 2;
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddTodoListItem([FromBody] AddTodoListItemCommand request)
+        {
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+
+        [HttpPut("{todoListItemId}")]
+        public async Task<ActionResult> UpdateTodoListItem([FromBody] UpdateTodoListItemCommand request)
+        {
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+
+        [HttpGet("{todoListItemId}")]
+        public async Task<ActionResult> GetTodoListItem([FromRoute] GetTodoListItemQuery request)
+        {
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{todoListItemId}")]
+        public async Task<ActionResult> DeleteTodoListItem([FromRoute] DeleteTodoListItemCommand request)
+        {
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpPut("{todoListItemId}/mark-as-done")]
+        public async Task<ActionResult> MarkAsDoneTodoListItem([FromRoute] MarkAsDoneTodoListItemCommand request)
+        {
             var result = await _mediator.Send(request);
 
             return Ok(result);

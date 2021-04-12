bfea652183ceff201f05ab6ed604de9323af6f1a
Sina Akbari <sina.akbari.victorion@outlook.com>
2021-04-13 05:42:18 +1000

    add abstract value accessor to set ngModel value

---
+++ TodoList.Web/ClientApp/src/app/core/abstracts/abstract-value-accessor.ts
@@ -0,0 +1,58 @@
+import { forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const noop = () => {
+};
+
+export abstract class AbstractValueAccessor implements ControlValueAccessor {
+  //The internal data model
+  private innerValue: any = null;
+
+  //Placeholders for the callbacks which are later provided
+  //by the Control Value Accessor
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  //get accessor
+  get value(): any {
+    return this.innerValue;
+  };
+
+  //set accessor including call the onchange callback
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  //Set touched on blur
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  //From ControlValueAccessor interface
+  writeValue(value: any) {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  //From ControlValueAccessor interface
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  //From ControlValueAccessor interface
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+}
+
+export function MakeProvider(type: any) {
+  return {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => type),
+    multi: true
+  };
+}
